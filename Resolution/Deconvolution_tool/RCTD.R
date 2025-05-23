3e4e149e1e868e2d20d0b15546cf8c09c4f4467b
2011CMML3 <15275977255@163.com>
2025-05-24 01:59:51 +0800

    Update RCTD.R

--- Resolution/Deconvolution_tool/RCTD.R
+++ Resolution/Deconvolution_tool/RCTD.R
@@ -1,1 +1,13 @@
+install.packages("devtools")
+devtools::install_github("dmcable/spacexr")
 
+library(spacexr)
+# load data
+reference <- Reference(counts = ref_data$counts, cell_types = ref_data$cell_types)
+spatial <- SpatialRNA(counts = spatial_data$counts, coords = spatial_data$coords)
+
+rctd <- create.RCTD(spatial, reference, max_cores = 4)
+rctd <- run.RCTD(rctd)
+
+results <- rctd@results
+weights <- results$weights
